3270e42c9f28ca6c09de9525dbdfc3daf93647f9
Kopa Ralte <kopa.ralte@gmail.com>
2025-06-07 17:47:51 +0000

    undo

--- src/app/stats/page.tsx
+++ src/app/stats/page.tsx
@@ -350,7 +350,7 @@ export default function EventStatsPage() {
                         margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
                         barCategoryGap="20%" 
                       >
-                        <CartesianGrid strokeDasharray="3 3" horizontal={true} vertical={false} /> 
+                        <CartesianGrid strokeDasharray="3 3" /> 
                         <XAxis type="number" tickLine={false} axisLine={false} tickMargin={8} allowDecimals={false} />
                         <YAxis 
                           dataKey="month" 
@@ -380,3 +380,4 @@ export default function EventStatsPage() {
     </div>
   );
 }
+
